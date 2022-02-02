c8de26eca07858fe72ce18483e1a75f5d0db7ea8
simran41 <kau10773@sheridancollege.ca>
2022-02-02 07:50:43 -0800

    commit to card.java file

--- src/ca/sheridancollege/week2/softwarefundamentals/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/Card.java
@@ -1,8 +1,8 @@
+package ca.sheridancollege.week2.softwarefundamentals;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ * To change this template 
 package ca.sheridancollege.week2.softwarefundamentals;
 
 /**
--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -1,19 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ modifier name:simranjeet kaur
+student number : 991634296
  */
 package ca.sheridancollege.week2.softwarefundamentals;
 
-/**
- * The class where we create Cards
- * @author dancye, 2019
- */
+
+
 public class CardDealer 
 {
     public static void main(String[] args)
     {
         Card nineClubs = new Card("Clbs", 9);
         nineClubs.setSuit("diamonds");
+        
+        Card c1 = new Card("hearts",2);
+        c1.setSuit("spades");
     }
+    
 }
