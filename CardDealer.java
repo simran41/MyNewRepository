a104fde509ff5245bc501f11511f7c27581027b1
simran41 <98033997+simran41@users.noreply.github.com>
2022-02-02 09:07:37 -0800

    Add files via upload

---
+++ CardDealer.java
@@ -0,0 +1,20 @@
+/*
+ modifier name:simranjeet kaur
+student number : 991634296
+ */
+package ca.sheridancollege.week2.softwarefundamentals;
+
+
+
+public class CardDealer 
+{
+    public static void main(String[] args)
+    {
+        Card nineClubs = new Card("Clbs", 9);
+        nineClubs.setSuit("diamonds");
+        
+        Card c1 = new Card("hearts",2);
+        c1.setSuit("spades");
+    }
+    
+}
